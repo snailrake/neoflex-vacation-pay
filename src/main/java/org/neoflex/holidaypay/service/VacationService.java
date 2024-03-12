c62e030468f505d6a06491b0d21e48d6f5427b14
Viktor Dyukov <snailrake@mail.ru>
2024-03-12 18:17:07 +0400

    refactor: minor changes

--- src/main/java/org/neoflex/holidaypay/service/VacationService.java
+++ src/main/java/org/neoflex/holidaypay/service/VacationService.java
@@ -9,9 +9,11 @@ import java.math.RoundingMode;
 @Service
 public class VacationService {
 
+    private static final double AVG_MONTH_DAYS = 29.3;
+
     public ResponseDto calculate(double avgSalary, int days) {
         return ResponseDto.builder()
-                .vacationPay(BigDecimal.valueOf(avgSalary / 29.3 * days).setScale(2, RoundingMode.HALF_EVEN))
+                .vacationPay(BigDecimal.valueOf(avgSalary / AVG_MONTH_DAYS * days).setScale(2, RoundingMode.HALF_EVEN))
                 .build();
     }
 }
